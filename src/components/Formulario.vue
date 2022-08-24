cc869c224c2f3b653dae4e01930a70f09ab1aa07
tashiro <andre_seichi@hotmail.com>
2022-08-24 17:16:02 -0300

    start

---
+++ src/components/Formulario.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário de criação de nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          name="input tarefa"
+          id=""
+          placeholder="Qual tarefa quer iniciar?"
+        />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong>{{ tempoDecorrido }}</strong>
+          </section>
+
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>Play</span>
+          </button>
+
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>Stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormularioComponent",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometroId: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido() {
+      return new Date(this.tempoEmSegundos * 1000)
+        .toISOString()
+        .substring(11, 19);
+    },
+  },
+  methods: {
+    iniciar() {
+      this.cronometroId = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      clearInterval(this.cronometroId);
+    },
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "@fortawesome/fontawesome-free/css/all.css";
+
+createApp(App).mount("#app");
