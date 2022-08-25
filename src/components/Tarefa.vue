85c57792c5fe7ec2bc589a2671c5e6d84ca789fa
tashiro <andre_seichi@hotmail.com>
2022-08-25 15:22:37 -0300

    pegando dinamicamente a tarefa e renderizando

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="box has-text-weight-bold">
     <div class="columns">
-      <div class="column is-7">Descrição da Tarefa</div>
+      <div class="column is-7">{{ tarefa.descricao }}</div>
       <div class="column">
-        <Cronometro tempoEmSegundos="15" />
+        <Cronometro :tempoEmSegundos="tarefa.duracao" />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { ITarefa } from "@/interfaces/ITarefa";
+import { defineComponent, PropType } from "vue";
 import Cronometro from "./Cronometro.vue";
 
 export default defineComponent({
   name: "TarefaItem",
   components: {
     Cronometro,
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
   },
 });
 </script>
