b3383b9d7c8a9484f7b188783a8eee2a087d81aa
tashiro <andre_seichi@hotmail.com>
2022-08-25 15:37:42 -0300

    componentizando box amarela

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BoxComponent",
+});
+</script>
+
+<style scoped>
+.box {
+  background-color: #faf0ca;
+}
+</style>
