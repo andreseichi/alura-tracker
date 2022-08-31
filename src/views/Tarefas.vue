9c049cda77fe8f3773ca176667f24d5814d85e72
tashiro <andre_seichi@hotmail.com>
2022-08-31 17:02:55 -0300

    adicionando filtro

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -2,7 +2,19 @@
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
     <Box v-if="isListaTarefaVazia"> Nenhuma tarefa realizada hoje :( </Box>
-
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input
+          class="input"
+          type="text"
+          placeholder="Filtrar as tarefas"
+          v-model="filtro"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa
       v-for="(tarefa, index) in tarefas"
       :key="index"
@@ -49,47 +61,26 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
-import Formulario from '../components/Formulario.vue';
-import Tarefa from '../components/Tarefa.vue';
-import Box from '../components/Box.vue';
+import { computed, defineComponent, ref } from "vue";
+import Formulario from "../components/Formulario.vue";
+import Tarefa from "../components/Tarefa.vue";
+import Box from "../components/Box.vue";
 
-import { ITarefa } from '../interfaces/ITarefa';
-import { useStore } from '@/store';
+import { ITarefa } from "../interfaces/ITarefa";
+import { useStore } from "@/store";
 import {
   CADASTRAR_TAREFA,
   OBTER_PROJETOS,
   OBTER_TAREFAS,
   ALTERAR_TAREFA,
-} from '@/store/tipo-acoes';
+} from "@/store/tipo-acoes";
 
 export default defineComponent({
-  name: 'TarefasView',
+  name: "TarefasView",
   components: { Formulario, Tarefa, Box },
   computed: {
     isListaTarefaVazia(): boolean {
       return this.tarefas.length === 0;
-    },
-  },
-  data: () => {
-    return {
-      tarefaSelecionada: null as ITarefa | null,
-    };
-  },
-  methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.store.dispatch(CADASTRAR_TAREFA, tarefa);
-    },
-    selecionarTarefa(tarefa: ITarefa) {
-      this.tarefaSelecionada = tarefa;
-    },
-    fecharModal() {
-      this.tarefaSelecionada = null;
-    },
-    editarTarefa() {
-      this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada).then(() => {
-        this.fecharModal();
-      });
     },
   },
   setup() {
@@ -97,9 +88,43 @@ export default defineComponent({
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
 
+    const tarefaSelecionada = ref<ITarefa | null>(null);
+
+    const tarefas = computed(() =>
+      store.state.tarefas.filter(
+        (tarefa) => !filtro.value || tarefa.descricao.includes(filtro.value)
+      )
+    );
+
+    const filtro = ref("");
+
+    const salvarTarefa = (tarefa: ITarefa) => {
+      store.dispatch(CADASTRAR_TAREFA, tarefa);
+    };
+
+    const selecionarTarefa = (tarefa: ITarefa) => {
+      tarefaSelecionada.value = tarefa;
+    };
+
+    const fecharModal = () => {
+      tarefaSelecionada.value = null;
+    };
+
+    const editarTarefa = () => {
+      store.dispatch(ALTERAR_TAREFA, tarefaSelecionada.value).then(() => {
+        fecharModal();
+      });
+    };
+
     return {
       store,
-      tarefas: computed(() => store.state.tarefas),
+      tarefas,
+      tarefaSelecionada,
+      salvarTarefa,
+      selecionarTarefa,
+      fecharModal,
+      editarTarefa,
+      filtro,
     };
   },
 });
